d9bae6fd0afbcaca0250a80d13646c740b9a6413
Red888 <114731391+Red888@users.noreply.github.com>
2023-10-01 18:42:03 +0800

    Add files via upload

--- mail.js
+++ mail.js
@@ -1,15 +1,15 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDqs53cnqVmYymvV7QOiaJQCczh2L9RoMM",
-    authDomain: "fir-form-8e52b.firebaseapp.com",
-    databaseURL: "https://fir-form-8e52b-default-rtdb.firebaseio.com",
-    projectId: "fir-form-8e52b",
-    storageBucket: "fir-form-8e52b.appspot.com",
-    messagingSenderId: "273343992682",
-    appId: "1:273343992682:web:6c8252d2bc181f17144b13"
-  };
+  apiKey: "AIzaSyDqs53cnqVmYymvV7QOiaJQCczh2L9RoMM",
+  authDomain: "fir-form-8e52b.firebaseapp.com",
+  databaseURL: "https://fir-form-8e52b-default-rtdb.firebaseio.com",
+  projectId: "fir-form-8e52b",
+  storageBucket: "fir-form-8e52b.appspot.com",
+  messagingSenderId: "273343992682",
+  appId: "1:273343992682:web:6c8252d2bc181f17144b13"
+};
 
 // initialize firebase
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 // reference your database
 var contactFormDB = firebase.database().ref('contactForm');
@@ -17,39 +17,39 @@ var contactFormDB = firebase.database().ref('contactForm');
 document.getElementById('contactForm').addEventListener("submit", submitForm);
 
 function submitForm(e){
-  e.preventDefault();
+e.preventDefault();
 
-  var name = getElementVal('name');
-  var emailid = getElementVal('emailid');
-  var msgContent = getElementVal('msgContent');
+var name = getElementVal('name');
+var emailid = getElementVal('emailid');
+var msgContent = getElementVal('msgContent');
 
-  saveMessage(name, emailid, msgContent);
+saveMessage(name, emailid, msgContent);
 
-  // enable alert message 
-  document.querySelector(".alert").style.display = "block";
+// enable alert message 
+document.querySelector(".alert").style.display = "block";
 
-  // remove the alert after 3 sec
-  setTimeout(() => {
-    document.querySelector(".alert").style.display = "none";
-  }, 3000);
+// remove the alert after 3 sec
+setTimeout(() => {
+  document.querySelector(".alert").style.display = "none";
+}, 3000);
 
-  // clear the content form
-  document.getElementById("contactForm").reset();
+// clear the content form
+document.getElementById("contactForm").reset();
 
 }
 
 const saveMessage = (name, emailid, msgContent) => {
-  var newContactForm = contactFormDB.push();
+var newContactForm = contactFormDB.push();
 
-  newContactForm.set({
-    name: name,
-    email: emailid,
-    msgContent: msgContent,
-  });
+newContactForm.set({
+  name: name,
+  email: emailid,
+  msgContent: msgContent,
+});
 };
 
 const getElementVal = (id) => {
-  return document.getElementById(id).value;
+return document.getElementById(id).value;
 }
 
 
